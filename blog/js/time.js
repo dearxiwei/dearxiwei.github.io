1759f8addff437c3b57b12f44e40275ed195254c
亦可 <88919780+dearxiwei@users.noreply.github.com>
2022-10-23 14:55:30 +0800

    Add files via upload

---
+++ blog/js/time.js
@@ -0,0 +1,18 @@
+function show_date_time(){
+window.setTimeout("show_date_time()", 1000);
+BirthDay=new Date("10-23-2022 08:00:00");//建站日期
+today=new Date();
+timeold=(today.getTime()-BirthDay.getTime());
+sectimeold=timeold/1000
+secondsold=Math.floor(sectimeold);
+msPerDay=24*60*60*1000
+e_daysold=timeold/msPerDay
+daysold=Math.floor(e_daysold);
+e_hrsold=(daysold-e_daysold)*-24;
+hrsold=Math.floor(e_hrsold);
+e_minsold=(hrsold-e_hrsold)*-60;
+minsold=Math.floor((hrsold-e_hrsold)*-60);
+seconds=Math.floor((minsold-e_minsold)*-60);
+momk.innerHTML=daysold+"天"+hrsold+"小时"+minsold+"分"+seconds+"秒" ;
+}
+show_date_time();
